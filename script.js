4d0a875c34c2864ec5816ded8a88e3128da8c3da
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-04-19 16:58:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
-    //Write your code here
+const inps = document.querySelectorAll('input');
+const sub = document.querySelector("#sub");
+sub.addEventListener("click",getFormvalue);
 
+	function getFormvalue() {
+    //Write your code here
+    event.preventDefault();
+		let firstName = inps[0].value;
+		let lastName = inps[1].value;
+            alert(firstName+" "+lastName);
 }
